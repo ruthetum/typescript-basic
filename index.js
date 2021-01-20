bc2a9e6580a10f267da58ff90733b1b4e751b6c9
Heedong Kim <ruthetum@gmail.com>
2021-01-21 04:11:38 +0900

    variable, function

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "heedong", age = 25, gender = "male";
+const sayHello = (name, age, gender) => {
+    console.log(`Hello ${name}, age is ${age}, gender is ${gender}`);
+};
+sayHello(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "heedong",
+    age = 25,
+    gender = "male";
+
+const sayHello = (name, age, gender) => {
+    console.log(`Hello ${name}, age is ${age}, gender is ${gender}`);
+}
+
+sayHello(name, age, gender);
+
+export {}
