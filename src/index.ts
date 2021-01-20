60d882d187233a5ef478868deba8e2616c7e0715
Heedong Kim <ruthetum@gmail.com>
2021-01-21 05:16:00 +0900

    practice - making block 3

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,6 @@ class Block {
 
     static calculateHash = (
         index: number,
-        hash: string,
         previousHash: string,
         data: string,
         timestamp: number
@@ -40,7 +39,15 @@ const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
 
+const createNewBlock = (data: string): Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimestamp: number = getNewTimestamp();
+    const newHash: string = Block.calculateHash(newIndex, previousBlock.hash, data, newTimestamp);
+    const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
+} ;
 
-console.log(blockchain);
+console.log(createNewBlock("hello"));
 
 export {}
